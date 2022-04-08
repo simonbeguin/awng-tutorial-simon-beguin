9d17da3c6e8edc36e75903340a0d44dd97c672e7
Simon Beguin <simon.beguin@student.isae-supaero.fr>
2022-04-08 20:44:37 +0200

    Erreurs 404 et création template detail

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from unittest import loader
-from django.shortcuts import render
 from django.template import loader
+from django.shortcuts import get_object_or_404, render
+
 
 
 # Create your views here.
@@ -15,7 +16,8 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
