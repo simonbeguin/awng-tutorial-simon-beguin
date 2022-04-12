17c852ea8d5faca361e3fc425266bd8e2fa26e82
Simon Beguin <simon.beguin@student.isae-supaero.fr>
2022-04-12 10:35:14 +0200

    Création d'un formulaire dans l'appli polls

---
+++ mysite/polls/forms.py
@@ -0,0 +1,7 @@
+
+# Create your models here.
+
+from django import forms
+
+class NameForm(forms.Form):
+    your_name = forms.CharField(label='Your name', max_length=100)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,6 +6,26 @@ from django.utils import timezone
 
 from .models import Choice, Question
 
+from .forms import NameForm
+
+def get_name(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/thanks/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'polls/name.html', {'form': form})
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
